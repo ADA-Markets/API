781d24414dfb7973021ceaafe417a6d854f71269
Patrick Blake <paddyblake09@hotmail.com>
2024-11-27 08:00:51 +0000

    update constants naming

--- examples/client/src/constants.ts
+++ examples/client/src/constants.ts
@@ -1,11 +1,15 @@
-// AdaMarkets API Url
+// FETCH API Url - request from team.
 export const baseUrl = '';
 
-// API Key
+// FETCH API Key - request from team.
 export const apiKey = '';
 
-//WALLET SEED PHRASE
+//WALLET SEED PHRASE & ADDRESS
 export const WALLET_SEED = "";//space seperated words
 export const WALLET_ADDRESS = "";
+
+// WALLET ADDRESS TO SEND AGGREGATION FEES
 export const FEE_WALLET_ADDRESS = "";
+
+// YOUR BLOCKFROST API KEY
 export const BLOCKFROST_KEY = "";
